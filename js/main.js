930cb734f46af4a707b93882aa30313a666ae7a4
fbecerra <becerrafernando@gmail.com>
2017-09-13 17:25:50 -0400

    First implementation of gaussian fit

--- js/main.js
+++ js/main.js
@@ -110,7 +110,8 @@ function init() {
             } else {
                 throw_disk = false;
                 // fit gaussian to all_points
-                console.log(all_points)
+                var fitted_data = fitGaussian(all_points);
+                plot.draw_fit(fitted_data, i, j)
             }
 
         }
@@ -488,6 +489,33 @@ function init() {
 
         };
 
+        this.draw_fit = function (data, i, j) {
+
+            var that = this;
+
+            this.line.x(function (d) {
+                    return that.x(d.x)
+                })
+                .y(function (d) {
+                    return that.y(d.y)
+                });
+
+            this.g.selectAll(".cell")
+                .filter(function(d){ return d.i === i & d.j === j;})
+                .append("path")
+                .datum(data)
+                .attr("class", "line")
+                .attr("fill", "none")
+                .attr("stroke", "red")
+                .attr("opacity", 1)
+                .attr("stroke-linejoin", "round")
+                .attr("stroke-linecap", "round")
+                .attr("stroke-width", 1.5)
+                .attr("d", this.line);
+
+
+        };
+
         function cross(a, b) {
             var c = [], n = a.length, m = b.length, i, j;
             for (i = -1; ++i < n;) for (j = -1; ++j < m;) c.push({x: a[i], i: i, y: b[j], j: j});
@@ -512,6 +540,70 @@ function init() {
         };
 
     }
+
+    function fitGaussian(data){
+        console.log(data);
+
+        // WE NEED TO NORMALIZE DATA!!
+
+        var model = function (a, x) {
+            var i, j, result = [], sig2 = a[1] * a[1], norm;
+            norm = a[0] / Math.sqrt(2 * Math.PI * sig2);
+
+            x = optimize.vector.atleast_1d(x);
+            a = optimize.vector.atleast_1d(a);
+
+            for (i = 0; i < x.length; i++) {
+                var diff = x[i] - a[2];
+                result.push(norm * Math.exp(-0.5 * diff * diff / sig2));
+            }
+
+            for (j = 3; j < a.length; j++) {
+                for (i = 0; i < x.length; i++) {
+                    result[i] += a[j] * Math.pow(x[i], j - 3);
+                }
+            }
+
+            return result;
+        };
+        var i, p1, chi;
+        var order = 0;
+        var xrange = d3.extent(data, function(d){ return d.x; });
+
+        var p0 = [100, 10, 10, 10] //d3.median(data, function (d) { return d.y; })];
+        for (i = 1; i <= order; i++) {
+            p0.push(0.0);
+        }
+
+        chi = function (p) {
+            var i, chi = [];
+            if (Math.abs(p[1]) > (xrange[1] - xrange[0]) ||
+                p[2] > xrange[1] || p[2] < xrange[0]) {
+                for (i = 0; i < data.length; i++) {
+                    chi.push(1e10);
+                }
+            }
+            for (i = 0; i < data.length; i++) {
+                chi.push((data[i].y - model(p, data[i].x)[0]));
+            }
+            return chi;
+        };
+        chi2 = function (p) {
+            var c = chi(p);
+            return optimize.vector.dot(c, c);
+        };
+        p1 = optimize.newton(chi, p0);
+        //p1 = optimize.fmin(chi2, p0);
+
+        var fit_data = [];
+        for (i = xrange[0]; i <= xrange[1]; i += (xrange[1] - xrange[0]) / 500.) {
+            fit_data.push({x: i, y: model(p1, i)[0]});
+        }
+
+        return fit_data;
+    }
+
+
 }
 
 window.onload = init;
